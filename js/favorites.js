19a24f2b4411ecef9453bc0b79aa1cf1efd7a3d3
Ismael-Figueiredo <maelio@live.com>
2023-12-06 07:52:19 -0300

    primeira etapa, comunicação com a api interna

--- js/favorites.js
+++ js/favorites.js
@@ -1,8 +1,24 @@
 export class Favorites{
     constructor(root){
-        this.root = document.querySelector(root)
+        this.root = document.querySelector(root) 
+        this.load()
         
-        this.update()
+    }
+    load(){
+        this.entries = [   
+            {
+                login: 'ismael-figueiredo',
+                name: 'Ismael fogueiredo',
+                public_repos: '75',
+                follower:'400'
+            },
+            {
+                login: 'maykbrito',
+                name: 'mayk brito',
+                public_repos: '75',
+                follower:'40010'
+            }
+        ]
     }
 
 }
@@ -11,14 +27,61 @@ export class Favorites{
 export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
+        this.tbody = this.root.querySelector('table tbody')
+
+        this.update()
     }
 
     update(){ 
-        const tbody = this.root.querySelector('table tbody')
-        tbody.querySelectorAll('tr').forEach((tr) => {
-            tr.remove()
-        });
+        this.removeAllTr()
+
+        this.entries.forEach( user => {
+           const row = this.createRow()
+           row.querySelector('.user img').alt =`Imagem do ${user.name}`
+           row.querySelector('.user img').src =`https://github.com/${user.login}.png`
+           row.querySelector('.user p').textContent = user.name 
+           row.querySelector('.user span').textContent = `/${user.login}`
+           row.querySelector('.repositories').textContent = user.public_repos
+           row.querySelector('.followers').textContent = user.follower
+            
+           this.tbody.append(row)
+    } )
+    
+    
     }
+
+    removeAllTr(){
+       
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
+            tr.remove()
+        })
+    }
+
+    createRow() {
+        const tr = document.createElement('tr')
+
+        
+        const content = `<tr>
+        <td class="user">
+            <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
+            <a href="https://github.com/maykbrito" target="_blank">
+                <p>Mayk Brito</p>
+                <span>/maykbrito</span>
+            </a>
+        </td>
+        <td class="repositories">
+            76
+        </td>
+        <td class="followers">
+            9589
+            </td>
+        <td>
+            <button class="acao">Remover</button>
+        </td>
+            </tr>`
+        tr.innerHTML = content
+        return tr
+        }
 
 }
    
--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
+
 import { FavoritesView } from "./favorites.js"
+import { minhaImagem, mybutton } from "./hover.js"
 
 
 
 
-const minhaImagem = document.getElementById('star-button');
-const mybutton = document.getElementById('search-button');
-
-mybutton.addEventListener('mouseover', function() {
-    minhaImagem.src = './assets/star-button-white.svg'; // Muda a imagem quando o mouse está sobre o botão
-});
-
-mybutton.addEventListener('mouseout', function() {
-    minhaImagem.src = './assets/star-button.svg'; // Volta para a imagem original quando o mouse sai do botão
-});
-
-new FavoritesView("app")
+new FavoritesView("#app")
